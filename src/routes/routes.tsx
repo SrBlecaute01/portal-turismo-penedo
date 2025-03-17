5223a0e33d60f791e3c1777aa673efd6c61f3a5a
José Arnaldo <josearnaldoma18k@gmail.com>
2025-03-17 07:10:35 -0300

    fix: fixing github pages incompatibility with react routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {HashRouter, Route, Routes} from "react-router-dom";
 import {lazy, Suspense} from "react";
 import Loading from "../components/Loading";
 
@@ -7,14 +7,14 @@ const HotelsPage = lazy(() => import("../pages/Hotels"))
 
 function AppRouter() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Suspense fallback={<Loading/>}>
         <Routes>
           <Route path="/" element={<HomePage/>}/>
           <Route path="/hoteis" element={<HotelsPage/>}/>
         </Routes>
       </Suspense>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
