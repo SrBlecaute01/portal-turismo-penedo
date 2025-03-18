7c199d2473ff7775a2ce4b8d991da5c62e81f305
SrBlecaute01 <SrBlecaute01@users.noreply.github.com>
2025-03-18 17:30:30 +0000

    Deploy preview for PR 2 🛫

---
+++ pr-preview/pr-2/assets/index-Bg0FvUow.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-CrWMsjNd.js";import{N as t}from"./Navbar-Cjw1FZAa.js";function e(){return r.jsx(r.Fragment,{children:r.jsx(t,{})})}export{e as default};
--- pr-preview/pr-2/assets/index-C7z9B5iQ.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-DSVxuNxu.js";import{N as t}from"./Navbar-BvSJO80j.js";function a(){return r.jsx(r.Fragment,{children:r.jsx(t,{})})}export{a as default};
