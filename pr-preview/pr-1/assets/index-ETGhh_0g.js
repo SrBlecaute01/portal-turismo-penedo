667ee0051f1cac729ddf9bd69653a369c488ddb0
SrBlecaute01 <SrBlecaute01@users.noreply.github.com>
2025-03-18 19:20:57 +0000

    Remove preview for PR 1 🛬

--- pr-preview/pr-1/assets/index-ETGhh_0g.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-DDnoE7sU.js";import{N as t}from"./Navbar-B2PnKxNn.js";function s(){return r.jsx(r.Fragment,{children:r.jsx(t,{})})}export{s as default};
--- pr-preview/pr-1/assets/index-GlNvTyNz.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-DDnoE7sU.js";import{N as t}from"./Navbar-B2PnKxNn.js";function a(){return r.jsx(r.Fragment,{children:r.jsx(t,{})})}export{a as default};
