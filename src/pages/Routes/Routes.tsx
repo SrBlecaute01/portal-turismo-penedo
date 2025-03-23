53a3b3340e5d529594e80a59e228b65b6e206d31
José Arnaldo <josearnaldoma18k@gmail.com>
2025-03-23 07:24:33 -0300

    feat: adding carousel component

--- src/pages/Routes/Routes.tsx
+++ src/pages/Routes/Routes.tsx
@@ -1,9 +1,15 @@
 import Navbar from "../../components/Navbar";
+import Carousel from "../../components/Carousel";
+
+const images = Object.values(import.meta.glob('../../assets/carousel/routes/*.{png,jpg,jpeg,svg}', { eager: true })) as { default: string }[];
 
 function Routes() {
   return (
     <>
       <Navbar/>
+      <div>
+        <Carousel images={images.map(image => image.default)}/>
+      </div>
     </>
   )
 }
