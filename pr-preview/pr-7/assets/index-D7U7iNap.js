b29736e9f2c8adb570e7db86228ba850777e3e15
SrBlecaute01 <SrBlecaute01@users.noreply.github.com>
2025-03-23 16:37:22 +0000

    Remove preview for PR 7 🛬

--- pr-preview/pr-7/assets/index-D7U7iNap.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./index-BnU2xU1O.js";var a=r();export{a as r};
--- pr-preview/pr-7/assets/index-Dk9V84E9.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-BnU2xU1O.js";import{N as t}from"./Navbar-BzfnNzYr.js";function a(){return r.jsx(r.Fragment,{children:r.jsx(t,{})})}export{a as default};
