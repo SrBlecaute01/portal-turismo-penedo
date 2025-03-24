429d1f0d4b29d2528f6ff6d2cd2cabc74fbed148
Ol1veirx <Ol1veirx@users.noreply.github.com>
2025-03-24 19:31:38 +0000

    Deploy preview for PR 10 🛫

---
+++ pr-preview/pr-10/assets/index-BEFtOlEK.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-DmiuwT2B.js";import{N as t}from"./Navbar-DrGO04tn.js";function a(){return r.jsx(r.Fragment,{children:r.jsx(t,{})})}export{a as default};
---
+++ pr-preview/pr-10/assets/index-C6snvD3m.js
@@ -0,0 +1,1 @@
+import{b as r}from"./index-DmiuwT2B.js";var a=r();export{a as r};
