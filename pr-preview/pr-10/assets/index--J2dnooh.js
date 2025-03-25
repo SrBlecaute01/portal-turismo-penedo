01834ec6e55dc19eff6112ba659a8f3e0116661e
gabrielfers <gabrielfers@users.noreply.github.com>
2025-03-25 01:30:29 +0000

    Remove preview for PR 10 🛬

--- pr-preview/pr-10/assets/index--J2dnooh.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-DmiuwT2B.js";import{N as t}from"./Navbar-DrGO04tn.js";function e(){return r.jsx(r.Fragment,{children:r.jsx(t,{})})}export{e as default};
--- pr-preview/pr-10/assets/index-BEFtOlEK.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-DmiuwT2B.js";import{N as t}from"./Navbar-DrGO04tn.js";function a(){return r.jsx(r.Fragment,{children:r.jsx(t,{})})}export{a as default};
