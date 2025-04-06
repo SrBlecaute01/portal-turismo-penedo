956da0dc10db71f38cefc933e9588d074c398384
TonyCyclone <a.gabriel.profissional@gmail.com>
2025-04-06 14:13:44 -0300

    update: deleted comments and unused components on hotels.tsx

--- src/pages/Hotels/Hotels.tsx
+++ src/pages/Hotels/Hotels.tsx
@@ -2,7 +2,6 @@ import Navbar from "../../components/Navbar";
 import SliderCard from "../../components/SliderCards/index.ts";
 import Carousel from "../../components/Carousel";
 import styles from './Hotels.module.css';
-import Searchbar from "../../components/Searchbar/Searchbar.tsx";
 import RatingCard from "../../components/RatingCard/RatingCard.tsx";
 
 const images = Object.values(import.meta.glob('../../assets/carousel/routes/*.{png,jpg,jpeg,svg}', { eager: true })) as { default: string }[];
@@ -21,10 +20,9 @@ function Hotels() {
       }}
     />
   )));
-
   cards_to_scroll = cards_to_scroll.concat(cards_to_scroll); // Duplicate the array to make it scrollable
   cards_to_scroll = cards_to_scroll.concat(cards_to_scroll); // Duplicate the array to make it scrollable again
-  /**/
+  
   return (
     /* Hotel Container*/
     <div className={styles.hotelsContainer}>
@@ -34,37 +32,12 @@ function Hotels() {
       {/* Carousel component*/}
       <Carousel text = 'Principais Destinos' images={images.map(image => image.default)}/>
 
-
-
       {/* Main Content Container*/}
       <div className={styles.hotelsContent}>
-        {/* Old Slider code
-        <div className={styles.slider_cards}>
-          <SliderCard cards={cards_to_scroll}/>
-        </div>
-        */}
         {/* New Slider code using Swiper component*/}
-        <SliderCard 
-          text="Mais procurados"
-          cards={cards_to_scroll}
-        />
-        {/* Old and responsive Cards List
-        <div className={styles.card_list}>
-          <div className={styles.horizontal_cards}>
-            <Card title="Hotel Exemplo Calipso" to="#" stars= {4.5} imageUrl="https://placehold.co/150x100/3388EE/FFFFFF/"/>
-            <Card title="Hotel Exemplo Calipso" to="#" description={description} isExpanded={true}/>
-          </div>
-          <div className={styles.horizontal_cards}>
-            <Card title="Hotel Exemplo Calipso" to="#" stars= {1} imageUrl="https://placehold.co/150x100/EE3388/FFFFFF/"/>
-            <Card title="Hotel Exemplo Calipso" to="#" description={description} isExpanded={true}/>
-          </div>
-          <div className={styles.horizontal_cards}>
-            <Card title="Hotel Exemplo Calipso" to="#" stars= {3} imageUrl="https://placehold.co/150x100/88EE33/FFFFFF/"/>
-            <Card title="Hotel Exemplo Calipso" to="#" description={description} isExpanded={true}/>
-          </div>
-        </div>
-        */}
-        {/* New Card list with placeholder cards */}
+        <SliderCard text="Mais procurados" cards={cards_to_scroll}/>
+
+        {/* Card list with placeholder cards */}
         <div className={styles.cardsListContainer}>
           {images.map(image =>
             <RatingCard
