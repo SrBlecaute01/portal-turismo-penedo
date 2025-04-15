55ab63a92eb0a0c15ef0032a8487898bcd92b3b2
TonyCyclone <a.gabriel.profissional@gmail.com>
2025-04-15 18:06:01 -0300

    feat: adicionadas informações sobre os hotéis; rating cards agora têm suporte navlink para redirecionamento, e descrição do rating card suporta elementos ReactNode além de string.

--- src/components/RatingCard/RatingCard.tsx
+++ src/components/RatingCard/RatingCard.tsx
@@ -1,9 +1,10 @@
 import styles from "./RatingCard.module.css";
 import {Rating, RatingProps} from "@mui/material";
+import {NavLink, To} from "react-router-dom";
 
 export interface RatingCardInfoProps {
   title: string;
-  description: string;
+  description: string | React.ReactNode;
 }
 
 export interface RatingCardImageProps {
@@ -13,6 +14,7 @@ export interface RatingCardImageProps {
 }
 
 export interface RatingCardProps {
+  to?: To;
   text?: string;
   className?: string;
   image: RatingCardImageProps;
@@ -30,20 +32,29 @@ const defaultRatingProps: RatingProps = {
 
 function RatingCard(props: RatingCardProps) {
   const ratingProps = { ...defaultRatingProps, ...props.rating };
+  const toLink = props.to ? props.to : "#";
   return(
     <div className={styles.ratingContainer + " " + (props.className ?? "")}>
       <div className={styles.cardContainer}>
         <img className={styles.cardImage + " " + (props.image.className ?? "")} src={props.image.image} alt={props.image.title}/>
         <div className={styles.cardContent}>
-          <div className={styles.cardText}>{props.text}</div>
+          <div className={styles.cardText}>
+            <NavLink to = {toLink}>
+              {props.text}
+            </NavLink>
+          </div>
           <Rating {...ratingProps}/>
         </div>
       </div>
 
       {(props.info) &&
           <div className={styles.infoContainer}>
-              <div className={styles.infoTitle}>{props.info.title}</div>
-              <div className={styles.infoDescription}>{props.info.description}</div>
+            <div className={styles.infoTitle}>
+              <NavLink to = {toLink}>
+                {props.info.title}
+              </NavLink>
+            </div>
+            <div className={styles.infoDescription}>{props.info.description}</div>
           </div>
       }
     </div>
--- src/pages/Hotels/Hotels.tsx
+++ src/pages/Hotels/Hotels.tsx
@@ -3,38 +3,112 @@ import SliderCard from "../../components/SliderCards/index.ts";
 import Carousel from "../../components/Carousel";
 import styles from './Hotels.module.css';
 import RatingCard from "../../components/RatingCard/RatingCard.tsx";
-import { useNavigate } from "react-router-dom";
 
 const images = Object.values(import.meta.glob('../../assets/carousel/routes/*.{png,jpg,jpeg,svg}', { eager: true })) as { default: string }[];
 
+const hotelImages = Object.values(import.meta.glob('../../assets/hotels/*.{png,jpg,jpeg,svg}', { eager: true })) as { default: string }[];
+
+const hoteis =[
+  {
+    id: 1,
+    title: "Hotel Encantos de Penedo",
+    to: "#",
+    description: 
+      <span>
+        Situado na Rodovia AL-110, nº 195, bairro Santa Luzia, em Penedo (AL), o Hotel Encantos de Penedo é uma hospedagem que combina conforto e praticidade.<br/>
+        Os quartos são amplos, higienizados e equipados com ar-condicionado, TV de tela plana, frigobar e banheiro privativo. O café da manhã é um dos destaques, sendo variado e incluindo tapiocas e sanduíches preparados na hora.\n <br/>
+        O hotel oferece estacionamento gratuito, Wi-Fi gratuito e serviço de quarto, com opção de jantar no apartamento. O check-in é a partir das 14h e o check-out até às 12h. A localização estratégica permite fácil acesso a mercados, lojas e postos de gasolina. A equipe é conhecida pela cordialidade e prestatividade, tornando a estadia ainda mais agradável. Ideal para viajantes que buscam tranquilidade e boa localização em Penedo.
+      </span>,
+    image: hotelImages[0].default
+  },
+  {
+    id: 2,
+    title: "Pousada Toca do Vale",
+    to: "#",
+    description: 
+      <span>
+        Situada no Residencial Vale do São Francisco, nº 10, bairro Dom Constantino, em Penedo (AL), a Pousada e Restaurante Toca do Vale combina hospedagem acolhedora com uma experiência gastronômica regional.<br/>
+        O restaurante funciona diariamente das 11h às 20h, oferecendo almoço e jantar com opções à la carte e self-service, destacando pratos típicos como galinha caipira. <br/>
+        A pousada oferece acomodações equipadas com ar-condicionado e estacionamento próprio para maior comodidade dos hóspedes. O check-in é realizado a partir das 14h e o check-out até as 12h. Com ambiente familiar e atendimento atencioso, é uma excelente opção para quem deseja desfrutar da culinária local e de uma estadia confortável em Penedo.
+      </span>,
+    image: hotelImages[1].default
+  },
+  {
+    id: 3,
+    title: "Hotel O Laçador",
+    to: "#",
+    description: 
+      <span>
+        Situada na Rodovia Engenheiro Joaquim Gonçalves, nº 17, bairro Cohab, em Penedo (AL), a Pousada e Churrascaria O Laçador combina hospedagem acolhedora com uma experiência gastronômica regional<br/>
+        O hotel oferece quartos bem decorados, equipados com ar-condicionado, TV via satélite, frigobar e banheiro privativo, estacionamento privativo gratuito, Wi-Fi gratuito em toda a propriedade e é pet friendly. Além disso, os hóspedes têm acesso gratuito a uma piscina localizada a 2 km do hotel. <br/>
+        O check-in é a partir das 12h e o check-out até às 12h. Ideal para viajantes que buscam tranquilidade, boa localização e excelente custo-benefício em Penedo.
+      </span>,
+    image: hotelImages[2].default
+  },
+  {
+    id: 4,
+    title: "Hotel São Francisco",
+    to: "#",
+    description: 
+      <span>
+        Localizado em uma das áreas mais tradicionais e encantadoras da cidade, o Hotel São Francisco está situado na Av. Floriano Peixoto, 237 - Centro Histórico, Penedo(AL), 57200-000. A poucos passos dos principais pontos turísticos e com fácil acesso ao Rio São Francisco, o hotel oferece uma estadia acolhedora, ideal tanto para turistas quanto para viajantes a trabalho.<br/>
+        As acomodações do Hotel São Francisco foram planejadas para garantir o máximo de conforto. Os quartos contam com ar-condicionado, Wi-Fi gratuito, e são decorados de forma simples, porém aconchegante, proporcionando um ambiente tranquilo e agradável para descansar.
+      </span>,
+    image: hotelImages[3].default
+  },
+  {
+    id: 5,
+    title: "Cellya's Pousada",
+    to: "#",
+    description: 
+      <span>
+        Situada na Rodovia Engenheiro Joaquim Gonçalves, nº 1115, em Penedo (AL), a Cellya's Pousada é uma excelente escolha para viajantes que buscam conforto e praticidade.<br/>
+        As suítes são bem equipadas, contando com ar-condicionado, TV de tela plana com canais via satélite, frigobar, Wi-Fi de alta velocidade e chuveiro quente. A área de lazer inclui piscina, churrasqueira e mesa de sinuca, criando um ambiente ideal para momentos de descanso e convivência entre amigos, casais ou famílias.<br/>
+        O café da manhã é servido diariamente das 6h20 às 9h30, oferecendo uma variedade de opções para atender a diferentes gostos. <br/>
+        O check-in é a partir das 12h e o check-out até as 12h. Com uma estrutura simples e funcional, a Cellya's Pousada oferece uma estadia agradável para quem visita Penedo.
+      </span>,
+    image: hotelImages[4].default
+  },
+  {
+    id: 6,
+    title: "Pousada Colonial Penedo",
+    to: "#",
+    description: 
+      <span>
+        A Pousada Colonial Penedo está localizada na Praça Doze de Abril, 21, Centro Histórico, em Penedo (AL), oferecendo uma experiência aconchegante e confortável para os visitantes que desejam explorar a charmosa cidade.<br/>
+        A pousada dispõe de quartos equipados com ar-condicionado e privativo, com toalhas e roupa de cama fornecidas, além de um guarda-roupa para acomodar seus pertences e Wi-Fi gratuito em toda a área. Além disso, a pousada possui quartos adequados para famílias, tornando-a uma excelente opção para aqueles que viajam com crianças.<br/> 
+        A estrutura também conta com restaurante e bar, onde é possível desfrutar de uma deliciosa refeição ou um bom drink em um ambiente descontraído. E a recepção da pousada funciona 24 horas por dia, sempre disposta a fornecer dicas sobre os melhores passeios e atividades na região.
+      </span>,
+    image: hotelImages[5].default
+  },
+  {
+    id: 7,
+    title: "Hotel Rochedo",
+    to: "#",
+    description: 
+      <span>
+        Localizado na Rodovia Engenheiro Joaquim Gonçalves, nº 700, bairro Dom Constantino, em Penedo (AL), o Hotel Rochedo combina conforto e praticidade para seus hóspedes.<br/>
+        Com 38 quartos bem equipados, oferece ar-condicionado, TV a cabo, frigobar, Wi-Fi gratuito e varanda. O café da manhã é servido diariamente, proporcionando uma variedade de opções para diferentes gostos. A área de lazer conta com piscina ao ar livre e terraço, ideal para momentos de relaxamento. <br/>
+        O hotel dispõe de estacionamento privativo gratuito e recepção 24 horas, garantindo conveniência e segurança. Além disso, é pet friendly, permitindo a hospedagem de animais de estimação. Com uma localização estratégica, oferece fácil acesso aos principais pontos turísticos da cidade. O check-in é a partir das 14h e o check-out até as 12h. Ideal para viajantes que buscam uma estadia confortável e bem localizada em Penedo.
+      </span>,
+    image: hotelImages[6].default
+  }
+];
+
 function Hotels() {
 
-  const description = "Lorem ipsum calypso voutom arumtac acadan çarcurt irfi cardeb oa poisqu andoche gonopa rámesin tobemo tempov oache gouom êsdef ériasv ouvo andop ropar ávoudi retal verop êsoapu rarmeu paladarfi carbemàvo tadee fazer oquequi zer ema tarmin hasau dade da pupun hacomca féeu vou naesta çãoda sdocas vouve rorexp anoes tádio vousa iránoi tecom osami goseu voume jogare uvou lano mangal dasgar çasvou nofor tedop resépio ede poisdo pointd oaçaí euque romediv ertir"
-  
   /* Card "Array" for current placeholding */
-  let cards_to_scroll = (images.map(image => (
+  let cards_to_scroll = (hoteis.map(hotel => (
     <RatingCard
-      text="Hotel Lorem Calipsum"
+      text={hotel.title}
       image={{
-        title: "Hotel Image",
-        image: image.default
+        title: "Imagem do " + hotel.title,
+        image: hotel.image
       }}
+      to={hotel.to}
     />
   )));
-  cards_to_scroll = cards_to_scroll.concat(cards_to_scroll); // Duplicate the array to make it scrollable
-  cards_to_scroll = cards_to_scroll.concat(cards_to_scroll); // Duplicate the array to make it scrollable again
  
-  const navigate = useNavigate();
-
-  const handleCardClick = (hotelId?: number, hotelName?: string) => {
-    if(Boolean(hotelId)){
-      navigate(`/hoteis/${hotelId}`);
-    }
-    else if(Boolean(hotelName)){
-      navigate(`/hoteis/${hotelName}`);
-    }
-  };
-  
   return (
     /* Hotel Container*/
     <div className={styles.hotelsContainer}>
@@ -51,17 +125,17 @@ function Hotels() {
 
         {/* Card list with placeholder cards */}
         <div className={styles.cardsListContainer}>
-          {images.map((image, index) =>
-            <div key ={index} onClick={() => handleCardClick(index+1)}>
+          {hoteis.map((hotel) =>
+            <div key ={hotel.id}>
               <RatingCard
-                text="Hotel Lorem Calipsum"
+                text={hotel.title}
                 image={{
-                  title: "green iguana",
-                  image: image.default
+                  title: "Imagem do " + hotel.title,
+                  image: hotel.image
                 }}
                 info={{
-                  title: "Hotel Lorem Calipsum",
-                  description: description
+                  title: hotel.title,
+                  description: hotel.description
                 }}
               />
             </div>
